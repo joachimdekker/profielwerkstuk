aaf576eb39afe9b01e652dcefa535bc6f53dd546
TheCodeMage <jdekker3008@gmail.com>
2020-09-06 15:47:44 +0200

    Updated line class to support limited domain Update line class to support creation with 2 points

--- classes/line.py
+++ classes/line.py
@@ -1,5 +1,6 @@
 from Vector import Vector2D
-from typing import NewType
+from typing import NewType, Union
+import math
 
 linline = NewType("linline", object)
 
@@ -9,9 +10,10 @@ class linline:
     The linear line is a simplified version of the polynomial line 'polyline'.
     
     """
-    def __init__(self, rc: float, b: float) -> None:
+    def __init__(self, rc: float, b: float, limit=[-math.inf,math.inf]) -> None:
         self.rc = float(rc)
         self.b = float(b)
+        self.limit = limit
     
     @classmethod
     def fromString(cls, string: str, variable="x") -> linline:
@@ -29,6 +31,33 @@ class linline:
         rc = direction.y / direction.x
         b = direction.x * location.y - direction.y * location.x
         return cls(rc, b)
+    
+    @classmethod
+    def fromPoints(cls, a: Union[Vector2D, tuple], b: Union[Vector2D, tuple]) -> linline:
+        """Create a line from points with a limited domain.
+
+            Create a line from two vectors or tuples.
+            The domain will be limited to the two points including.
+
+            Args:
+                a: `Vector2D | tuple` Point A of the line
+                b: `Vector2D | tuple` Point B of the line
+
+            Returns:
+                `linline` A linear line with a limited domain.
+        """
+        if(type(a) == tuple):
+            a = Vector2D.fromTuple(a)
+
+        if(type(b) == tuple):
+            b = Vector2D.fromTuple(b)
+        
+        rc = (b.y - a.y)/(b.x - a.x)
+        p = a.y - rc * a.x
+        limit = [a.x, b.x]
+        print(f"Limit: {limit}")
+
+        return cls(rc, p, sorted(limit))
 
     def intersect(self, other: linline) -> Vector2D:
         """Calculate the point on which the two lines intersect
@@ -38,9 +67,16 @@ class linline:
 
         Args:
             other: a linline; the line that intersects
+
+        Returns:\n
+        `Vector2D` A vector if a match is found \n
+        `None` None if no match is found
         """
         new_x = (other.b - self.b) / (self.rc - other.rc)
-        return Vector2D(new_x, self.calc(new_x))
+        if self.limit[0] <= new_x <= self.limit[1] and other.limit[0] <= new_x <= other.limit[1]: 
+            return Vector2D(new_x, self.calc(new_x))
+        else:
+            return None
 
     def calc(self, x: float) -> float:
         """Calculate the output with a given x value
@@ -51,16 +87,26 @@ class linline:
         Args:
             x: a float; the x value for the value to be calculated
         """
+        if(self.limit): #Check if the line is limited
+            if not self.limit[0] <= x <= self.limit[1]: #Check if the domain contains the value of x
+                raise ValueError(f"Domain Error: x value does not meet the domain ({self.limit})") 
+
         return self.rc * x + self.b
-        
+
     def __str__(self):
-        return f"Linear Line: {self.rc}x + {self.b}"
+        return f"Linear Line: {self.rc}x + {self.b} -> Limit: {self.limit}"
     
     def __repr__(self):
-        return f"linline({self.rc}x + {self.b})"
+        return f"linline({self.rc}x + {self.b}, limit={self.limit})"
 
 if __name__ == "__main__":
-    l1 = linline(1,2)
+    l1 = linline(1,2, [1,3])
     l2 = linline.fromString("3x+1")
+    print(l1)
+    print(l2)
     print(l1.intersect(l2))
+
+    l3 = linline.fromPoints((-2,-1),(3,1))
+    print(l3.intersect(linline(1,0)))
+    print(-math.inf <= -4 <= math.inf)
 
